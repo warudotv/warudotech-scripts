f6cf0be0dd5b95bc439a899068e3ad12deffdb7d
Pedro Silva Ferreira <106485981+warudotv@users.noreply.github.com>
2026-06-11 15:51:12 -0300

    Add files via upload

---
+++ remover_setup.ps1
@@ -0,0 +1,9 @@
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+[System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
+function remover_warudo { 
+    $confirm = Read-Host "Tem certeza que deseja remover as automações Warudo? (S/N)"
+    if ($confirm -eq 'S') {
+        (Get-Content $PROFILE) | Where-Object { $_ -notmatch 'ytmp4|ytmp3' } | Set-Content $PROFILE
+        Write-Host "Automações removidas com sucesso! Reinicie o PowerShell." -ForegroundColor Green
+    }
+}
