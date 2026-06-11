0a1d1ac4ec344acd844647bb65796d7c148b189c
Pedro Silva Ferreira <106485981+warudotv@users.noreply.github.com>
2026-06-11 06:41:54 -0300

    Refactor PowerShell script for installing packages

--- setup.ps1
+++ setup.ps1
@@ -5,28 +5,22 @@ Add-AppxPackage $installer
 
 Remove-Item $installer -Force
 
-Write-Host "Instalando FFmpeg (necessário para processamento)..." -ForegroundColor Cyan
-winget install Gyan.FFmpeg
-
 Write-Host "Instalando o yt-dlp via Winget..." -ForegroundColor Cyan
 winget install yt-dlp.yt-dlp
 
 Write-Host "Configurando o seu perfil do PowerShell..." -ForegroundColor Cyan
-if (!(Test-Path -Path $PROFILE)) { 
-    New-Item -Type File -Path $PROFILE -Force 
-}
+if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force };
 
-# Defina aqui o seu navegador (chrome, edge, firefox, opera, brave, vivaldi)
 $browser = "chrome"
 
-$functions = @"
+$functions = @'
 function ytmp4 { yt-dlp -o "`$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata --merge-output-format mp4 `$args }
 function ytmp4_ck { yt-dlp --cookies-from-browser $browser -o "`$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata --merge-output-format mp4 `$args }
 function ytmp3_320 { yt-dlp -o "`$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata `$args }
 function ytmp3_320_ck { yt-dlp --cookies-from-browser $browser -o "`$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata `$args }
 function ytmp3_128 { yt-dlp -o "`$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata `$args }
 function ytmp3_128_ck { yt-dlp --cookies-from-browser $browser -o "`$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata `$args }
-"@
+'@
 
 $functions | Out-File -FilePath $PROFILE -Append
 Write-Host "Ok! Reinicie o PowerShell." -ForegroundColor Green
