b21a9972632cf3f35bbb9ce9e5c5eef6924a1594
Pedro Silva Ferreira <106485981+warudotv@users.noreply.github.com>
2026-06-26 16:23:58 -0300

    Update setup script for Aráudo Tech

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,7 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 [System.Console]::InputEncoding = [System.Text.Encoding]::UTF8
-Write-Host "--- Script de Setup Automatizado do WARUDO ---" -ForegroundColor Yellow
+
+Write-Host "--- Script de Setup Automatizado do Aráudo Tech ---" -ForegroundColor Yellow
 
 Write-Host "Baixando e instalando o Winget..." -ForegroundColor Cyan
 $installer = "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
@@ -19,7 +20,7 @@ if ($confirmFirefox -eq 'S' -or $confirmFirefox -eq 's') {
 Start-Sleep -Seconds 3
 
 Write-Host "Instalando o yt-dlp e suas dependencias..." -ForegroundColor Cyan
-winget install yt-dlp.yt-dlp Gyan.FFmpeg denoland.deno
+winget install -e --id yt-dlp.yt-dlp
 Start-Sleep -Seconds 3
 
 Write-Host "--- AVISO IMPORTANTE ---" -ForegroundColor Red
@@ -33,12 +34,12 @@ Start-Sleep -Seconds 3
 if (!(Test-Path -Path $PROFILE )) { New-Item -Type File -Path $PROFILE -Force };
 
 $functions = @'
-function ytmp4 { yt-dlp -o "$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata --merge-output-format mp4 $args }
-function ytmp4_ck { yt-dlp -o "$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:default-release --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata --merge-output-format mp4 $args }
-function ytmp3_320 { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata $args }
-function ytmp3_320_ck { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:default-release --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata $args }
-function ytmp3_128 { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --windows-filenames --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata $args }
-function ytmp3_128_ck { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:default-release --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata $args }
+function ytmp4 { yt-dlp -o "$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata --merge-output-format mp4 $args }
+function ytmp4_ck { yt-dlp -o "$env:USERPROFILE\Videos\ytdlp\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:perfil --sleep-interval 5 --max-sleep-interval 15 -S "vcodec:h264,res,acodec:m4a" --embed-thumbnail --embed-metadata $args }
+function ytmp3_320 { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata $args }
+function ytmp3_320_ck { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\320k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:perfil --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 320K --ppa "ExtractAudio:-ar 48000" --embed-thumbnail --embed-metadata $args }
+function ytmp3_128 { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata $args }
+function ytmp3_128_ck { yt-dlp -o "$env:USERPROFILE\Music\ytdlp\128k\%(playlist_title,uploader)s/%(title)s - %(uploader)s.%(ext)s" --cookies-from-browser firefox:perfil --sleep-interval 5 --max-sleep-interval 15 -x --audio-format mp3 --audio-quality 128K --embed-thumbnail --embed-metadata $args }
 '@
 
 $functions | Out-File -FilePath $PROFILE -Append
